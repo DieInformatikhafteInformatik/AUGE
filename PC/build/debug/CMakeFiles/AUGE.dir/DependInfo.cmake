3d3c07920f197a1b6b8e642799fe959d17dbcc1d
Joel <joel.vallejo-direktor.bg@bs-gn.de>
2025-11-07 06:34:03 +0100

    update

---
+++ PC/build/debug/CMakeFiles/AUGE.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Joel/Documents/EGK-Projekt/AUGE/PC/src/auge.cpp" "CMakeFiles/AUGE.dir/src/auge.cpp.obj" "gcc" "CMakeFiles/AUGE.dir/src/auge.cpp.obj.d"
+  "C:/Users/Joel/Documents/EGK-Projekt/AUGE/PC/src/command.cpp" "CMakeFiles/AUGE.dir/src/command.cpp.obj" "gcc" "CMakeFiles/AUGE.dir/src/command.cpp.obj.d"
+  "C:/Users/Joel/Documents/EGK-Projekt/AUGE/PC/src/main.cpp" "CMakeFiles/AUGE.dir/src/main.cpp.obj" "gcc" "CMakeFiles/AUGE.dir/src/main.cpp.obj.d"
+  "C:/Users/Joel/Documents/EGK-Projekt/AUGE/PC/src/stringutils.cpp" "CMakeFiles/AUGE.dir/src/stringutils.cpp.obj" "gcc" "CMakeFiles/AUGE.dir/src/stringutils.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ PC/build/debug/CMakeFiles/AUGE.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "AUGE.exe"
+  "AUGE.exe.manifest"
+  "AUGE.pdb"
+  "CMakeFiles/AUGE.dir/src/auge.cpp.obj"
+  "CMakeFiles/AUGE.dir/src/auge.cpp.obj.d"
+  "CMakeFiles/AUGE.dir/src/command.cpp.obj"
+  "CMakeFiles/AUGE.dir/src/command.cpp.obj.d"
+  "CMakeFiles/AUGE.dir/src/main.cpp.obj"
+  "CMakeFiles/AUGE.dir/src/main.cpp.obj.d"
+  "CMakeFiles/AUGE.dir/src/stringutils.cpp.obj"
+  "CMakeFiles/AUGE.dir/src/stringutils.cpp.obj.d"
+  "libAUGE.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AUGE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
